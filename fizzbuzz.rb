5edde59dc0d004e1c8b8d0fdd8059e365bf2443a
Christopher Wood <cjwood032@gmail.com>
2019-04-11 19:19:31 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,5 @@
 def fizzbuzz(int)
-  int %3 == 0 && int % 5 == 0  
+  int %3 == 0 && int % 5 == 0 ? "FizzBuzz" : ( int % 3 == 0 ? "Fizz" : ( int % 5 == 0 ? "Buzz" : )) 
   if int % 3 == 0 && int % 5 == 0
     "FizzBuzz"
   elsif int % 3 == 0
